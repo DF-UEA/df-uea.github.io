5e0b0c1e7923fafaeb8f908b501bb977b60fded3
DF-UEA <99736587+DF-UEA@users.noreply.github.com>
2024-10-03 11:36:08 +0100

    Add files via upload

--- wau-mods/Google.Chrome-installed.ps1
+++ wau-mods/Google.Chrome-installed.ps1
@@ -19,9 +19,29 @@ if (Test-Path -Path "$PrefFile" -PathType Leaf) {
 	    If ($json) {
     	    If ($json.distribution -ne $null) {
         	    $json.distribution | Add-Member -Name "do_not_create_desktop_shortcut" -Value $true -MemberType NoteProperty -Force
+				$json.distribution | Add-Member -Name "do_not_create_any_shortcuts" -Value $true -MemberType NoteProperty -Force
+				$json.distribution | Add-Member -Name "do_not_create_taskbar_shortcut" -Value $true -MemberType NoteProperty -Force
+				$json.distribution | Add-Member -Name "create_all_shortcuts" -Value $false -MemberType NoteProperty -Force
+				$json.distribution | Add-Member -Name "skip_first_run_ui" -Value $true -MemberType NoteProperty -Force
+				$json.distribution | Add-Member -Name "show_welcome_page" -Value $false -MemberType NoteProperty -Force
+				$json.distribution | Add-Member -Name "import_search_engine" -Value $false -MemberType NoteProperty -Force
+				$json.distribution | Add-Member -Name "import_history" -Value $false -MemberType NoteProperty -Force
+				$json.distribution | Add-Member -Name "do_not_launch_chrome" -Value $true -MemberType NoteProperty -Force
+				$json.distribution | Add-Member -Name "make_chrome_default" -Value $false -MemberType NoteProperty -Force
+				$json.distribution | Add-Member -Name "system_level" -Value $true -MemberType NoteProperty -Force
     	    }
     	    Else {
         	    $json | Add-Member -Name "distribution" -Value @{"do_not_create_desktop_shortcut"=$true} -MemberType NoteProperty -Force
+				$json | Add-Member -Name "distribution" -Value @{"do_not_create_any_shortcuts"=$true} -MemberType NoteProperty -Force
+				$json | Add-Member -Name "distribution" -Value @{"do_not_create_taskbar_shortcut"=$true} -MemberType NoteProperty -Force
+				$json | Add-Member -Name "distribution" -Value @{"create_all_shortcuts"=$false} -MemberType NoteProperty -Force
+				$json | Add-Member -Name "distribution" -Value @{"skip_first_run_ui"=$true} -MemberType NoteProperty -Force
+				$json | Add-Member -Name "distribution" -Value @{"show_welcome_page"=$false} -MemberType NoteProperty -Force
+				$json | Add-Member -Name "distribution" -Value @{"import_search_engine"=$false} -MemberType NoteProperty -Force
+				$json | Add-Member -Name "distribution" -Value @{"import_history"=$false} -MemberType NoteProperty -Force
+				$json | Add-Member -Name "distribution" -Value @{"do_not_launch_chrome"=$true} -MemberType NoteProperty -Force
+				$json | Add-Member -Name "distribution" -Value @{"make_chrome_default"=$false} -MemberType NoteProperty -Force
+				$json | Add-Member -Name "distribution" -Value @{"system_level"=$true} -MemberType NoteProperty -Force
     	    }
     	    $json | ConvertTo-Json -Depth 100 | Out-File -FilePath $PrefFile -Encoding ASCII
 	    }
