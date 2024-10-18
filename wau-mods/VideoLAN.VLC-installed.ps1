a67fa67be9018641c632fbf553743878331486a7
AW-UEA <vdb11fpu@uea.ac.uk>
2024-10-18 15:54:30 +0100

    Update VideoLAN.VLC-installed.ps1
    Updated shortcut to remove first-run pop-ups.

--- wau-mods/VideoLAN.VLC-installed.ps1
+++ wau-mods/VideoLAN.VLC-installed.ps1
@@ -1,9 +1,21 @@
+# Remove Start Menu Shortcut
+  Remove-Item 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\VideoLAN\VLC media player.lnk' -Force -ErrorAction SilentlyContinue
+
+# Create an Updated Start Menu Shortcut
+   $s=(New-Object -COM WScript.Shell).CreateShortcut('C:\ProgramData\Microsoft\Windows\Start Menu\Programs\VideoLAN\VLC media player.lnk');
+    $s.TargetPath          = 'C:\Program Files\VideoLAN\VLC\vlc.exe';
+    $s.Arguments           = '--no-qt-privacy-ask --no-qt-updates-notif';
+    $s.IconLocation        = 'C:\Program Files\VideoLAN\VLC\vlc.exe';
+    $s.WorkingDirectory    = 'C:\Program Files\VideoLAN\VLC\';
+    $s.WindowStyle         = 7;
+    $s.Save();
+
 # Remove Desktop shortcut
-Remove-Item "C:\Users\Public\Desktop\VLC media player.lnk" -Force -ErrorAction SilentlyContinue
+Remove-Item 'C:\Users\Public\Desktop\VLC media player.lnk' -Force -ErrorAction SilentlyContinue
 
 # Remove Start Menu shortcuts
-Remove-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\VideoLAN\Documentation.lnk" -Force -ErrorAction SilentlyContinue
-Remove-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\VideoLAN\Release Notes.lnk" -Force -ErrorAction SilentlyContinue
-Remove-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\VideoLAN\VideoLAN Website.lnk" -Force -ErrorAction SilentlyContinue
-Remove-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\VideoLAN\VLC media player - reset preferences and cache files.lnk" -Force -ErrorAction SilentlyContinue
-Remove-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\VideoLAN\VLC media player skinned.lnk" -Force -ErrorAction SilentlyContinue
+Remove-Item 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\VideoLAN\Documentation.lnk' -Force -ErrorAction SilentlyContinue
+Remove-Item 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\VideoLAN\Release Notes.lnk' -Force -ErrorAction SilentlyContinue
+Remove-Item 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\VideoLAN\VideoLAN Website.lnk' -Force -ErrorAction SilentlyContinue
+Remove-Item 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\VideoLAN\VLC media player - reset preferences and cache files.lnk' -Force -ErrorAction SilentlyContinue
+Remove-Item 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\VideoLAN\VLC media player skinned.lnk' -Force -ErrorAction SilentlyContinue
