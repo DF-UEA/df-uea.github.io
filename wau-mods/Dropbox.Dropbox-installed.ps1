b7f25345367ccbc04b7f2c30dee7f54b8845b6b0
DF-UEA <99736587+DF-UEA@users.noreply.github.com>
2024-10-21 14:58:59 +0100

    Add files via upload

---
+++ wau-mods/Dropbox.Dropbox-installed.ps1
@@ -0,0 +1,8 @@
+# Remove Start Menu shortcuts
+
+Remove-Item 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Dropbox\Dropbox Website.lnk' -Force -ErrorAction SilentlyContinue
+
+# Remove Dropbox Update Scheduled Tasks - Will rely on WAU keeping up to date
+
+Get-ScheduledTask -TaskName "DropboxUpdateTaskMachineCore" | Unregister-ScheduledTask -Confirm:$false
+Get-ScheduledTask -TaskName "DropboxUpdateTaskMachineUA" | Unregister-ScheduledTask -Confirm:$false
