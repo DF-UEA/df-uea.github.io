d7a43a5169ea519c445e8ac5574d197477fff0d3
DF-UEA <99736587+DF-UEA@users.noreply.github.com>
2024-10-21 15:34:17 +0100

    Update Dropbox.Dropbox-installed.ps1

--- wau-mods/Dropbox.Dropbox-installed.ps1
+++ wau-mods/Dropbox.Dropbox-installed.ps1
@@ -3,6 +3,5 @@
 Remove-Item 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Dropbox\Dropbox Website.URL' -Force -ErrorAction SilentlyContinue
 
 # Remove Dropbox Update Scheduled Tasks - Will rely on WAU keeping up to date
-
 Get-ScheduledTask -TaskName "DropboxUpdateTaskMachineCore" | Unregister-ScheduledTask -Confirm:$false
 Get-ScheduledTask -TaskName "DropboxUpdateTaskMachineUA" | Unregister-ScheduledTask -Confirm:$false
