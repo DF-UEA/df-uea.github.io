96606c489bc8fac5824fbc8fa25a881fb5b35f1a
DF-UEA <99736587+DF-UEA@users.noreply.github.com>
2025-01-10 17:01:39 +0000

    Delete wau-mods/Adobe.Acrobat.Reader.64-bit-uninstalled.ps1

--- wau-mods/Adobe.Acrobat.Reader.64-bit-uninstalled.ps1
+++
@@ -1,4 +0,0 @@
-# Remove a couple of Policies
-
-Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown" -Name "bAcroSuppressUpsell" -Force -ErrorAction SilentlyContinue
-Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown" -Name "bToggleFTE" -Force -ErrorAction SilentlyContinue
